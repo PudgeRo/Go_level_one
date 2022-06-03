503833bb2d3e3068cba82bb3e1d1035026918473
Roman <danilenko_roma@vk.com>
2022-06-04 01:24:53 +0300

    update

--- lesson8/conf/conf.go
+++ lesson8/conf/conf.go
@@ -1,30 +1,38 @@
 package conf
 
 import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
 	"os"
+	"strconv"
 )
 
 type Specification struct {
-	Port         int `default:"5432"`
-	DbUrl       string
-	Users        []string
-	SentryUrl   string
-	KafkaBroker map[string]int `default:"kafka:9092"`
-	Id           string
+	Port        int64          `default:"5432" required:"true"`
+	DbUrl       string         `default:"postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable" required:"true"`
+	Users       []string       `default:"" required:"false"`
+	SentryUrl   string         `required:"true"`
+	KafkaBroker map[string]int `required:"false"`
+	Id          string         `required:"false"`
 }
 
-func SetEnv() (users, port, dbUrl, sentryUrl, kafkaBroker, id string) {
+func SetEnv() (error, *Specification) {
 	os.Setenv("MYAPP_USERS", "Roman,Andrey,Dima")
 	os.Setenv("MYAPP_PORT", "5432")
-	os.Setenv("MYAPP_DBURL", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable")
+	if _, err := strconv.Atoi(os.Getenv("MYAPP_PORT")); err != nil {
+		return fmt.Errorf("Port must be type of int"), nil
+	}
+	os.Setenv("MYAPP_DBRL", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable")
 	os.Setenv("MYAPP_SENTRYURL", "http://sentry:9000")
+	if os.Getenv("MYAPP_SENTRYURL") == "" {
+		return fmt.Errorf("Sentry url cannot be empty"), nil
+	}
 	os.Setenv("MYAPP_KAFKABROKER", "kafka:9092")
 	os.Setenv("MYAPP_ID", "testid")
-	users = os.Getenv("MYAPP_USERS")
-	port = os.Getenv("MYAPP_PORT")
-	dbUrl = os.Getenv("MYAPP_DB_URL")
-	sentryUrl = os.Getenv("MYAPP_SENTRY_URL")
-	kafkaBroker = os.Getenv("MYAPP_KAFKA_BROKER")
-	id = os.Getenv("MYAPP_ID")
-	return 
+	conf := Specification{}
+	err := envconfig.Process("myapp", &conf)
+	if err != nil {
+		return fmt.Errorf("can't process the config: %w", err), nil
+	}
+	return nil, &conf
 }
--- lesson8/main/main.go
+++ lesson8/main/main.go
@@ -4,34 +4,31 @@ import (
 	"Go_level_one/lesson8/conf"
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/kelseyhightower/envconfig"
 )
 
 func main() {
-	var s conf.Specification
-	conf.SetEnv()
-	err := envconfig.Process("myapp", &s)
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	err, config := conf.SetEnv()
 	if err != nil {
-		log.Fatal(err.Error())
+		panic(err)
 	}
-
-    os.Setenv("MYAPP_SENTRY_URL", "http://sentry:9000")
-
-	format := "Port: %v\nDb url: %v\nSentry url: %s\nId: %s\n"
-	_, err = fmt.Printf(format, s.DbUrl, s.Port, s.SentryUrl, s.Id)
+	format := "Port: %v\nDb url: %v\nSentry url: %v\nId: %v\n"
+	_, err = fmt.Printf(format, config.DbUrl, config.Port, config.SentryUrl, config.Id)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Users:")
-	for _, u := range s.Users {
-		fmt.Printf("  %s\n", u)
+	for _, u := range config.Users {
+		fmt.Printf("  %v\n", u)
 	}
 
 	fmt.Println("Kafka broker:")
-	for k, v := range s.KafkaBroker {
-		fmt.Printf("  %s: %d\n", k, v)
+	for k, v := range config.KafkaBroker {
+		fmt.Printf("  %v: %v\n", k, v)
 	}
 }
