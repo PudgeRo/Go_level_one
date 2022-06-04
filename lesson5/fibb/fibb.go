d0cecd70877e73a0d88820c22be3a5ad8b600a0f
Roman <danilenko_roma@vk.com>
2022-06-04 22:25:36 +0300

    update fibb

--- lesson5/fibb/fibb.go
+++ lesson5/fibb/fibb.go
@@ -1,16 +1,43 @@
 package fibb
 
-func Fibonacci(number int) int {
-	if number == 0 || number == 1 {
-		return number
+import "errors"
+
+var ErrNonPositive = errors.New("number must be positive")
+
+func Fibonacci(number int) (int, error) {
+	if number < 0 {
+		return 0, ErrNonPositive
 	}
-	return Fibonacci(number - 1) + Fibonacci(number - 2)
+	if number == 0 || number == 1 {
+		return number, nil
+	}
+	a, err := Fibonacci(number - 1)
+	if err != nil {
+		return 0, err
+	}
+	b, err := Fibonacci(number - 2)
+	if err != nil {
+		return 0, err
+	}
+	result := a + b
+	return result, nil
 }
 
-func FibonacciWithOptimization(number int, cache map[int]int) int {
-	if val, ok := cache[number]; ok {
-		return val
+func FibonacciWithOptimization(number int, cache map[int]int) (int, error) {
+	if number < 0 {
+		return 0, ErrNonPositive
 	}
-	cache[number] = FibonacciWithOptimization(number - 1, cache) + FibonacciWithOptimization(number - 2, cache)
-	return cache[number]
+	if val, ok := cache[number]; ok {
+		return val, nil
+	}
+	a, err := FibonacciWithOptimization(number-1, cache)
+	if err != nil {
+		return 0, err
+	}
+	b, err := FibonacciWithOptimization(number-2, cache)
+	if err != nil {
+		return 0, err
+	}
+	cache[number] = a + b
+	return cache[number], nil
 }
