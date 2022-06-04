2aaa7c2de325fad2d1556372aaebbbb131200783
Roman <danilenko_roma@vk.com>
2022-06-04 22:30:13 +0300

    read json

---
+++ lesson9/conf/conf.go
@@ -0,0 +1,36 @@
+package conf
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Specification struct {
+	Port        string   `json:"port"`
+	DbUrl       string   `json:"dbUrl"`
+	Users       []string `json:"users"`
+	SentryUrl   string   `json:"sentryUrl"`
+	KafkaBroker string   `json:"kafkaBroker"`
+	Id          string   `json:"id"`
+}
+
+func SetEnv() (error, *Specification) {
+	confJson, err := os.Open("../conf/conf.json")
+	if err != nil {
+		return fmt.Errorf("Cannot open file: %v", err), nil
+	}
+	defer func() error {
+		err := confJson.Close()
+		if err != nil {
+			return fmt.Errorf("Cannot close file: %v", err)
+		}
+		return nil
+	}()
+	config := Specification{}
+	err = json.NewDecoder(confJson).Decode(&config)
+	if err != nil {
+		return fmt.Errorf("Cannot decode json file into structure: %v", err), nil
+	}
+	return nil, &config
+}
---
+++ lesson9/main/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"Go_level_one/lesson8/conf"
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic occurred:", err)
+		}
+	}()
+
+	err, config := conf.SetEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	err = envconfig.Process("myapp", config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	format := "Port: %v\nDb url: %v\nSentry url: %v\nId: %s\nKafka broker: %v\n"
+	_, err = fmt.Printf(format, config.DbUrl, config.Port, config.SentryUrl, config.Id, config.KafkaBroker)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Users:")
+	for _, u := range config.Users {
+		fmt.Printf("  %v\n", u)
+	}
+}
